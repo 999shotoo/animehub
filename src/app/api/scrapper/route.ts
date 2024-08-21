f6c5918141e8246e59477a4aa7e969e20575ce61
shotoo999 <mohitshedge5@gmail.com>
2024-08-21 18:28:54 +0530

    d

--- src/app/api/scrapper/route.ts
+++ src/app/api/scrapper/route.ts
@@ -1,18 +1,14 @@
 import { NextRequest, NextResponse } from 'next/server';
-import puppeteer from 'puppeteer';
+import { JSDOM } from 'jsdom';
 
 export async function GET(req: NextRequest) {
-    const browser = await puppeteer.launch({ browser: 'firefox' });
-  const page = await browser.newPage();
-  await page.goto('https://books.toscrape.com/');
-  const data = await page.evaluate(() => {
-    // Extract data from the page
-    const elements = Array.from(document.querySelectorAll('.product_pod'));
-    const responsse = elements.map((element: any) => ({
-        title: element.querySelector('h3 a').getAttribute('title'),
-    }));
-    return responsse;
-  });
-  await browser.close();
-  return NextResponse.json(data);
+
+    const ress = await fetch("https://www.npmjs.com/package/shoto");
+    const html = await ress.text();
+
+    const dom = new JSDOM(html);
+    const document = dom.window.document;
+
+    const downloads = document.querySelector("._9ba9a726")?.textContent;
+    return NextResponse.json({downloads: downloads});
 }
