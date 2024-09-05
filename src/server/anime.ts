78e51aa1692b2bd4dea74e8ce61e3b88694cd5e3
shotoo999 <mohitshedge5@gmail.com>
2024-09-05 14:41:47 +0530

    s

--- src/server/anime.ts
+++ src/server/anime.ts
@@ -1,53 +1,65 @@
 export async function FetchPopularAnime(){
-    const response = await fetch(`${process.env.SITE_URL}/api/anime/popular`, { next: { revalidate: 3600 } });
+    const url = new URL(`/api/anime/popular`, process.env.VERCEL_URL || 'http://localhost:3000');
+    const response = await fetch(url.href, { next: { revalidate: 3600 } });
     const data = await response.json();
     return data;
-}
-
-export async function FetchTrendingAnime(per_page: number){
-    const response = await fetch(`${process.env.SITE_URL}/api/anime/trending?perPage=${per_page}`, { next: { revalidate: 3600 } });
+  }
+  
+  export async function FetchTrendingAnime(per_page: number){
+    const url = new URL(`/api/anime/trending`, process.env.VERCEL_URL || 'http://localhost:3000');
+    url.searchParams.set('perPage', per_page.toString());
+    const response = await fetch(url.href, { next: { revalidate: 3600 } });
     const data = await response.json();
     return data;
-}
-
-export const FetchTopAnime = async (count:number) => {
-    const data = await fetch(`${process.env.SITE_URL}/api/anime/top?perPage=${count}`, { next: { revalidate: 3600 } });
+  }
+  
+  export const FetchTopAnime = async (count:number) => {
+    const url = new URL(`/api/anime/top`, process.env.VERCEL_URL || 'http://localhost:3000');
+    url.searchParams.set('perPage', count.toString());
+    const data = await fetch(url.href, { next: { revalidate: 3600 } });
     const episodeData = await data.json();
     return episodeData;
-}
-
-export const FetchNewAnime = async (count: number) => {
-    const data = await fetch(`${process.env.SITE_URL}/api/anime/latest?perPage=${count}`, { next: { revalidate: 3600 } })
-    return await data.json();
   }
-
   
-export const FetchGenerAnime = async (genre:string, count: number) => {
-    const data = await fetch(`${process.env.SITE_URL}/api/anime/gener/${genre}?page=1&perPage=${count}`)
+  export const FetchNewAnime = async (count: number) => {
+    const url = new URL(`/api/anime/latest`, process.env.VERCEL_URL || 'http://localhost:3000');
+    url.searchParams.set('perPage', count.toString());
+    const data = await fetch(url.href, { next: { revalidate: 3600 } })
     return await data.json();
   }
   
-
-export async function FetchInfoAnime(id: string){
-    const response = await fetch(`${process.env.SITE_URL}/api/anime/info/${id}`, { next: { revalidate: 3600 } });
+  export const FetchGenerAnime = async (genre:string, count: number) => {
+    const url = new URL(`/api/anime/gener/${genre}`, process.env.VERCEL_URL || 'http://localhost:3000');
+    url.searchParams.set('page', '1');
+    url.searchParams.set('perPage', count.toString());
+    const data = await fetch(url.href)
+    return await data.json();
+  }
+  
+  export async function FetchInfoAnime(id: string){
+    const url = new URL(`/api/anime/info/${id}`, process.env.VERCEL_URL || 'http://localhost:3000');
+    const response = await fetch(url.href, { next: { revalidate: 3600 } });
     const data = await response.json();
     return data;
-}
-
-export async function FetchInfoAnimeExtra(id: string){
-    const response = await fetch(`https://api.jikan.moe/v4/anime/${id}/full`, { next: { revalidate: 3600 } });
+  }
+  
+  export async function FetchInfoAnimeExtra(id: string){
+    const url = new URL(`https://api.jikan.moe/v4/anime/${id}/full`);
+    const response = await fetch(url.href, { next: { revalidate: 3600 } });
     const data = await response.json();
     return data;
-}
-
-export async function FetchEpisodesAnime(id: string){
-    const response = await fetch(`${process.env.SITE_URL}/api/anime/episodes/${id}`, { next: { revalidate: 3600 } });
+  }
+  
+  export async function FetchEpisodesAnime(id: string){
+    const url = new URL(`/api/anime/episodes/${id}`, process.env.VERCEL_URL || 'http://localhost:3000');
+    const response = await fetch(url.href, { next: { revalidate: 3600 } });
     const data = await response.json();
     return data;
-}
-
-export async function FetchEpisodesSrcAnime(episodeId: string){
-    const response = await fetch(`${process.env.SITE_URL}/api/anime/watch/${episodeId}`, { next: { revalidate: 3600 } });
+  }
+  
+  export async function FetchEpisodesSrcAnime(episodeId: string){
+    const url = new URL(`/api/anime/watch/${episodeId}`, process.env.VERCEL_URL || 'http://localhost:3000');
+    const response = await fetch(url.href, { next: { revalidate: 3600 } });
     const data = await response.json();
     return { data, status: response.status };
-}
+  }
--- src/server/search.ts
+++ src/server/search.ts
@@ -1,4 +1,9 @@
 export const FetchSearch = async (query:string, type:string, per_page:string) => {
-    const data = await fetch(`${process.env.SITE_URL}/api/search/advance?query=${query}&type=${type}&perPage=${per_page}`, { next: { revalidate: 3600 } });
-    return await data.json();
-  }
+  const url = new URL(`/api/search/advance`, process.env.SITE_URL);
+  url.searchParams.set('query', query);
+  url.searchParams.set('type', type);
+  url.searchParams.set('perPage', per_page);
+  const response = await fetch(url.href, { next: { revalidate: 3600 } });
+  const data = await response.json();
+  return data;
+}
