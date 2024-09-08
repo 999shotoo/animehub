1e5294bc9b909b9d2b2286b626900cc66d333a8c
shotoo999 <mohitshedge5@gmail.com>
2024-09-08 11:19:20 +0530

    added manga api

---
+++ src/app/api/manga/read/[id]/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { META } from '@consumet/extensions';
+
+const anilist = new META.Anilist.Manga();
+
+export async function GET(req: NextRequest, res: NextResponse) {
+  try {
+    const { pathname } = req.nextUrl;
+    const id = pathname.split('/').pop();
+
+    if (id === undefined) {
+      return NextResponse.json(new Error('Invalid id parameter'), {status:400}); 
+    }
+
+    const data = await anilist.fetchChapterPages(id);
+
+    if (!data) {
+      return NextResponse.json('Data not found');
+    }
+
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({error: "Something went wrong"}, {status:500});
+  }
+}
--- src/server/manga.ts
+++ src/server/manga.ts
@@ -1,6 +1,6 @@
 
-export async function FetchTrendingManga(per_page: number){
-    const response = await fetch(`${process.env.AnimeApi_1}/meta/anilist/advanced-search?type=MANGA&sort=["TRENDING_DESC"]&perPage=${per_page}`, { next: { revalidate: 3600 } });
+export async function FetchTrendingManga(count: number){
+    const response = await fetch(`${process.env.AnimeApi_1}/meta/anilist/advanced-search?type=MANGA&sort=["TRENDING_DESC"]&perPage=${count}`, { next: { revalidate: 3600 } });
     const data = await response.json();
     return data;
 }
